264f8977a5dd42850be3eaa81c2db7322f7675ce
xpheres <xpheres@gmail.com>
2015-04-08 21:11:08 +0200

    update

--- app/src/main/java/com/example/root/analyticaltranslator/MainActivity.java
+++ app/src/main/java/com/example/root/analyticaltranslator/MainActivity.java
@@ -1,39 +1,74 @@
 package com.example.root.analyticaltranslator;
 
-import android.support.v7.app.ActionBarActivity;
+import org.json.JSONObject;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private TextView txtDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtDisplay = (TextView) findViewById(R.id.edittext_german);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://lingoworld.eu/lingoworld/translator/website/php/translatorfunction.php";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                // TODO Auto-generated method stub
+                txtDisplay.setText("Response => "+response.toString());
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+        queue.add(jsObjRequest);
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onclickactivity(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://lingoworld.eu/lingoworld/translator/website/php/translatorfunction.php";
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            @Override
+            public void onResponse(JSONObject response) {
+                // TODO Auto-generated method stub
+                txtDisplay.setText("Response => "+response.toString());
+            }
+        }, new Response.ErrorListener() {
 
-        return super.onOptionsItemSelected(item);
-    }
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+        queue.add(jsObjRequest);
+    };
 }
